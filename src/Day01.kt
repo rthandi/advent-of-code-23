5cc27af65ccded26a1b34a9f783213681ffab108
rthandi <robbie.thandi@gmail.com>
2023-12-01 20:40:47 +0000

    day 1 task 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,14 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { string -> string.filter { it.isDigit() } }.map { it.take(1) + it.takeLast(1) }
+            .sumOf { it.toInt() }
     }
 
     fun part2(input: List<String>): Int {
         return input.size
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInput("inputs/day_01")
     part1(input).println()
     part2(input).println()
 }
